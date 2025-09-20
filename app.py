312a75cc14f3afaeaa250d37cd9544881a622068
waynelmccoy <waynemccoy@email.phoenix.edu>
2025-09-20 13:47:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -420,4 +420,5 @@ def save_notes(n, notes, sim_state):
     return sim_state
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
